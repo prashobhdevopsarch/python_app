44fe225a5e6724c89f72a265de0ede412b155823
blueartcloud <prashobhchandra@artcl.cloud>
2024-09-13 09:46:17 +0530

    cicd python

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '''
+    <html>
+        <body>
+            <h1 style="color: blue; font-family: Arial, sans-serif; text-align: center;">
+                Hello, <span style="color: red;">World!</span>
+            </h1>
+        </body>
+    </html>
+    '''
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
